5b7ca42d1577d69da02f979257ca99ad85bf8e9f
Sebastian M. Pedersen <sem.pedersen@stud.uis.no>
2016-04-16 17:57:21 +0200

    Updated readme and package doc

---
+++ doc.go
@@ -0,0 +1,10 @@
+/*
+Package talktalk is for sending messages between nodes and clients.
+Messages between nodes are sent using UDP.
+Messages between a node and a client are sent using websocket.
+All communication is unreliable, meaning, if messages somehow fail to be sent or received, those messages are simply dropped.
+It's the node's/client's job to retransmit messages that are not answered.
+
+This package was created to be used as the network layer between Paxos replicas and its clients.
+*/
+package talktalk
